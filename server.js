69b67e79d3bb729558ee76527ce02c94a98ffe7e
thyhates <thyhates@gmail.com>
2017-04-19 21:45:35 +0800

    init project

---
+++ server.js
@@ -0,0 +1,15 @@
+/**
+ * Created by thyhates on 2017/4/19.
+ */
+const express = require("express");
+const compression = require('compression');
+const fs = require("fs");
+const app = express();
+app.use(compression());
+app.use(express.static(__dirname + "/node_modules"));
+app.use(express.static(__dirname ));
+app.use(express.static(__dirname + "/src"));
+app.use(express.static(__dirname + "/dist"));
+app.listen(8087, function () {
+    console.log("It's express,welcome!  http://127.0.0.1:8087");
+});
