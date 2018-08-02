ffae606b097286eb844e11047a11e11b0b39326d
thyhates <thyhates@gmail.com>
2018-08-02 17:25:18 +0800

    redesign queue

--- queue.js
+++ queue.js
@@ -1,95 +1,128 @@
 /**
- * Created by thyhates on 2017/4/24.
+ * Initialize your data structure here. Set the size of the queue to be k.
+ * @param {number} k
  */
-function Queue() {
-    let items=[];
-    this.enqueue=(element)=>{
-        items.push(element);
-    };
-    this.dequeue=()=>items.shift();
-    this.front=()=>items[0];
-    this.isEmpty=()=>items.length===0;
-    this.size=()=>items.length;
-    this.print=()=>{
-        console.log(items.toString());
-    };
-    this.clear=()=>{
-        items=[];
-    };
-}
+var MyCircularQueue = function (k) {
+    this._queue = new Array(k);
+    this._length = k;
+    this._head = -1;
+    this._tail = -1;
+    this._size = 0;
+};
 
-module.exports=Queue;
-let queue=new Queue();
-console.log(queue.isEmpty());
-queue.enqueue('John');
-queue.enqueue('Jack');
-queue.enqueue('Camila');
-queue.print();
-console.log(queue.size());
-console.log(queue.isEmpty());
-queue.dequeue();
-queue.dequeue();
-queue.print();
-function PriorityQueue() {
-    let items=[];
-    function QueueElement(element, priority) {
-        this.element=element;
-        this.priority=priority;
+/**
+ * Insert an element into the circular queue. Return true if the operation is successful.
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function (value) {
+    if(this.isEmpty()){
+        this._head++;
+        this._tail++;
+        this._queue[this._tail] = value;
+        this._size++;
+        return true;
+    }else if(this.isFull()){
+        return false;
+    }else{
+        this._tail++;
+        if(this._tail===this._length){
+            this._tail =0;
+        }
+        this._queue[this._tail] = value;
+        this._size++;
+        return true;
     }
-    this.enqueue=function (element, priority) {
-        let queueElement=new QueueElement(element,priority);
-        if(this.isEmpty()){
-            items.push(queueElement);
+};
+
+/**
+ * Delete an element from the circular queue. Return true if the operation is successful.
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function () {
+    if(this.isEmpty()){
+        return false;
+    }else{
+        this._queue[this._head] = undefined;
+        if(this._head === this._tail){
+            this._head = -1;
+            this._tail = -1;
         }else{
-            let added=false;
-            for(let i=0;i<items.length;i++){
-                if(queueElement.priority<items[i].priority){
-                    items.splice(i,0,queueElement);
-                    added=true;
-                    break;
-                }
-            }
-            if(!added){
-                items.push(queueElement);
+            this._head++;
+            if(this._head===this._length){
+                this._head=0;
             }
         }
-    };
-    this.dequeue=()=>items.shift();
-    this.front=()=>items[0];
-    this.isEmpty=()=>items.length===0;
-    this.size=()=>items.length;
-    this.print=()=>{
-        let printQueue=items.map(function (item) {
-            return item.element;
-        });
-        console.log(printQueue.toString());
-    };
-    this.clear=()=>{
-        items=[];
-    };
-}
-let priorityQueue=new PriorityQueue();
-priorityQueue.enqueue('John',2);
-priorityQueue.enqueue('Jack',1);
-priorityQueue.enqueue('Camila',1);
-console.log('-'.repeat(15));
-priorityQueue.print();
-console.log('-'.repeat(20));
-function hotPotato(nameList, num) {
-    let queue=new Queue();
-    for(let i=0;i<nameList.length;i++){
-        queue.enqueue(nameList[i]);
+        this._size--;
+        return true;
     }
-    let eliminated='';
-    while(queue.size()>1){
-        for(let i=0;i<num;i++){
-            queue.enqueue(queue.dequeue());
+};
+
+/**
+ * Get the front item from the queue.
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function () {
+    return this._queue[this._head]===undefined?-1:this._queue[this._head];
+};
+
+/**
+ * Get the last item from the queue.
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function () {
+    return this._queue[this._tail]===undefined?-1:this._queue[this._tail];
+};
+MyCircularQueue.prototype.clear = function () {
+    this._queue = new Array(this._length);
+    this._size = 0;
+};
+/**
+ * Checks whether the circular queue is empty or not.
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function () {
+    return (this._head === -1 && this._tail === -1)
+};
+
+/**
+ * Checks whether the circular queue is full or not.
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function () {
+    if(this._tail===(this._length-1)){
+        return this._head===0;
+    }else{
+        var tail = this._tail+1;
+        if(tail===this._length){
+            tail = 0;
         }
-        eliminated=queue.dequeue();
-        console.log(eliminated+'在击鼓传花游戏中被淘汰');
+        return tail===this._head;
     }
-    return queue.dequeue();
-}
-let names=['John','Jack','Camila','Ingrid','Carl'];
-let winner=hotPotato(names,7);
-console.log('获胜者:'+winner);
+};
+MyCircularQueue.prototype.size = function () {
+    return this._size;
+};
+// Your MyCircularQueue object will be instantiated and called as such:
+var queue = new MyCircularQueue(4);
+console.log(queue.enQueue(3));
+console.log('size:',queue.size());
+console.log(queue.Front());
+console.log(queue.isFull());
+console.log(queue.enQueue(7));
+console.log('size:',queue.size());
+console.log(queue.enQueue(2));
+console.log('size:',queue.size());
+console.log(queue.enQueue(5));
+console.log('size:',queue.size());
+console.log(queue.deQueue());
+console.log('size:',queue.size());
+console.log(queue.enQueue(4));
+console.log('size:',queue.size());
+console.log(queue.enQueue(2));
+console.log('size:',queue.size());
+console.log(queue.isEmpty());
+console.log(queue.Rear());
+console.log(queue.clear());
+console.log('size:',queue.size());
+console.log(queue.Rear());
